687567cf81b3e73b7f61f9afa759da7cb19035de
RemyWILHELM <remy.wilhelm2@gmail.com>
2023-02-02 00:03:44 +0100

    Changement final

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,7 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './TodoListBasic.css';
+
+interface ListItem {
+    id: string;
+    label: string;
+}
 
 const TodoListBasic = () => {
-    return <div>TodoListBasic</div>;
-};
+    const [todo, setTodo] = useState<ListItem[]>([]);
+    const [inProgress, setInProgress] = useState<ListItem[]>([]);
+    const [done, setDone] = useState<ListItem[]>([]);
+    const [newItemName, setNewItemName] = useState<string>('');
+    const [newItemCategory, setNewItemCategory] = useState<string>('todo');
 
+    const randomId = () => (Math.random() + 1).toString(36).substring(7);
+
+    const handleOnNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewItemName(e.target.value);
+    };
+
+    const handleOnCategoryChange = (
+        e: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setNewItemCategory(e.target.value);
+    };
+
+    const handleOnClick = () => {
+        const newItem = {
+            id: randomId(),
+            label: newItemName,
+        };
+        switch (newItemCategory) {
+            case 'todo':
+                setTodo([...todo, newItem]);
+                break;
+            case 'inProgress':
+                setInProgress([...inProgress, newItem]);
+                break;
+            case 'done':
+                setDone([...done, newItem]);
+                break;
+        }
+
+        setNewItemName('');
+        setNewItemCategory('todo');
+    };
+
+    return (
+        <div className="todo-list-basic">
+            <div className="todo-list-basic-add">
+                <input onChange={handleOnNameChange} value={newItemName} />
+                <select
+                    onChange={handleOnCategoryChange}
+                    value={newItemCategory}
+                >
+                    <option value="todo">To do</option>
+                    <option value="inProgress">In progress</option>
+                    <option value="done">Done</option>
+                </select>
+                <button disabled={!newItemName.length} onClick={handleOnClick}>
+                    Add to list
+                </button>
+            </div>
+
+            <div className="todo-list-basic-columns">
+                <div>
+                    <span>To do</span>
+                    <ul>
+                        {todo.map(({ id, label }) => (
+                            <li key={id}>{label}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div>
+                    <span>In progress</span>
+                    <ul>
+                        {inProgress.map(({ id, label }) => (
+                            <li key={id}>{label}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div>
+                    <span>Done</span>
+                    <ul>
+                        {done.map(({ id, label }) => (
+                            <li key={id}>{label}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
 export default TodoListBasic;
--- src/TodoListWithDesign/TodoListWithDesign.tsx
+++ src/TodoListWithDesign/TodoListWithDesign.tsx
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
-import {  Input, Button, Select } from 'antd';
+import {  List, Card, Input, Button, Select } from 'antd';
 import './TodoListWithDesign.css';
 
+interface ListColumn {
+    idColumn: string;
+    columnLabel: string;
+}
 
+interface ListItem {
+    id: string;
+    idCol: string
+    Itemlabel: string;
+}
 
 const TodoListWithDesign = () => {
 
+
+    const [Column, setColumn] = useState<ListColumn[]>([]);
+    const [unableButton, setUnableButton] = useState<boolean>(false);
     const [newColumnName, setNewColumnName] = useState<string>('');
+    const [newItemCategoryID, setNewItemCategoryID] = useState<string>('');
     const [newItemName, setNewItemName] = useState<string>('');
-    const [newItemCategory, setNewItemCategory] = useState<string>('todo');
+    const [Item, setItem] = useState<ListItem[]>([]);
 
     const randomId = () => (Math.random() + 1).toString(36).substring(7);
 
@@ -20,25 +33,72 @@ const TodoListWithDesign = () => {
         setNewItemName(e.target.value);
     };
 
-    const handleChange = () => {
-        
-      };
-      const handleColumnClick = () => {
+    const handleColumnClick = () => {
+        const newColumn = {
+            idColumn:randomId(),
+            columnLabel:newColumnName,
         };
+
+        setColumn([...Column, newColumn]); 
+
+    };
+
+    const handleChange = (value: string ) => {
+        setUnableButton(true);
+        setNewItemCategoryID(value);
+        console.log(value);
+    };
+
+    const handleItemClick = () => {
+        const newItem = {
+            id: randomId(),
+            idCol: newItemCategoryID,
+            Itemlabel: newItemName,
+        };
+        setItem([...Item,newItem]);
+        
+    };
+
+    const data = [
+        
+        Column.map((columnLabel: string) => ({
+             title: columnLabel
+        }))
+          
+        
+      ];
+
     return (
         <div>
-        <div className="todo-list-design">
-        <Input onChange={handleOnColumnNameChange} value={newColumnName} placeholder="Column name" />
-        <Button disabled={!newItemName.length}  onClick={handleColumnClick}>Add column</Button>
-        </div>
-        <div className="todo-list-design">
-        <Input onChange={handleOnItemNameChange} value={newItemName} placeholder="Item name" />
-        <Select
-        onChange={handleChange}
-        placeholder="Select Column"
-        />
-        <Button disabled={!newItemName.length}>Add item</Button>
-        </div>
+            <div className="todo-list-design">
+            <Input onChange={handleOnColumnNameChange} value={newColumnName} placeholder="Column name" />
+            <Button disabled={!newColumnName.length}  onClick={handleColumnClick}>Add column</Button>
+            </div>
+            <div className="todo-list-design">
+            <Input onChange={handleOnItemNameChange} value={newItemName} placeholder="Item name" />
+            <Select
+            onChange={handleChange}
+            placeholder="Select Column"
+            options={
+                Column.map(({idColumn, columnLabel}) => ({
+                    value: idColumn, label: columnLabel
+                }))}
+            />
+            <Button disabled={!newItemName.length || unableButton===false} onClick={handleItemClick}>Add item</Button>
+            </div>
+            <div className="todo-list-design">
+            <List
+                grid={{
+                column: Column.length,
+                }}
+                renderItem={() => (
+                <List.Item>
+                    <Card title={data} >             
+                    </Card>
+                </List.Item>
+                )}
+            />
+            </div>
         </div>
     );
 };
